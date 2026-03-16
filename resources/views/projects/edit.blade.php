657cb13ccadced4cf1c28fc29cd56a7be371c9e8
Francesco Liuzzi <fliuzzi2004@gmail.com>
2026-03-16 18:23:26 +0100

    aaa

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <!--prova -->
+    <!--prova -->
     <a href="{{ route('projects.index') }}" class="btn btn-link p-0 mb-3">← Torna alla lista dei progetti</a>
     <div class="container">
         <h1>Modifica Progetto: {{ $project->title }}</h1>
